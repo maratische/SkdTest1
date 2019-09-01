814213f894d3d68417be243881290274eba19477
maratische <maratische@gmail.com>
2019-09-01 22:40:07 +0300

    git

---
+++ src/main/java/maratische/kotlin/sdktest1/CheckController.java
@@ -0,0 +1,17 @@
+package maratische.kotlin.sdktest1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CheckController {
+
+    @PostMapping("/check")
+    public String check(@RequestParam("roomId") Integer roomId,
+                        @RequestParam("entrance") Boolean entrance,
+                        @RequestParam("keyId") Integer keyId) {
+        return "Hello Gradle!";
+    }
+}
